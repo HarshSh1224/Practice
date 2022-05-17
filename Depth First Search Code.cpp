95a1ea409eacc9f370dc068bf6f725b065001e26
Harsh Sharma <102849523+HarshSh1224@users.noreply.github.com>
2022-05-17 14:07:08 +0530

    Create Depth First Search Code.cpp

---
+++ Depth First Search Code.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int N = 1e5+10;
+vector<int> graph[N];
+bool vis[N];
+
+void dfs(int vertex){
+
+    /*Take action on vertex after entering the vertex*/
+
+    vis[vertex] = true;
+    cout<<vertex<<endl;
+    for(auto child: graph[vertex]){
+    
+    /*Take action on child before entering the child node*/
+
+        cout<<vertex<<" "<<child<<endl;
+        if(vis[child]) continue;
+        dfs(child);
+
+    /*Take action on child after exiting the node*/
+
+    }
+
+    /*Take action on vertex before exiting the vertex*/
+}
+
+int main(){
+    int n,m=9;
+    cin>>n>>m;
+
+    for (int i = 0; i < m; ++i){
+        int v1,v2; cin>>v1>>v2;
+        graph[v1].push_back(v2);
+        graph[v2].push_back(v1);
+    }
+
+    dfs(1);
+
+}
